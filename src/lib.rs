9ac5cb99003a2b15b7b6608f08990ad5bb9facf5
Amit Prasad <mail@amitprasad.dev>
2022-06-19 19:12:44 -0400

    Expose decode/encode hex functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 pub mod errors;
 pub mod keys;
 pub mod proof;
-pub(crate) mod utils;
 pub(crate) mod traits;
+pub(crate) mod utils;
 
 pub use crate::{
     errors::VRFError,
     keys::{PublicKey, SecretKey},
     proof::Proof,
+    traits::{decode_hex, encode_hex},
 };
